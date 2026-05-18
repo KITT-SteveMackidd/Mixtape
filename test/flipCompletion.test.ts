12b76075b81b8a127aa5cc634068915cc01d7f39
Steve Mackidd <kitt.openclaw.stevemackidd@gmail.com>
2026-05-18 05:16:59 +0000

    test: tidy flip completion assertions

--- test/flipCompletion.test.ts
+++ test/flipCompletion.test.ts
@@ -3,64 +3,71 @@ import test from 'node:test';
 
 import { getFlipCompletionSideIndex } from '../src/utils/flipCompletion.ts';
 
+function assertFlipCompletionSideIndex(
+  input: Parameters<typeof getFlipCompletionSideIndex>[0],
+  expected: number,
+) {
+  assert.equal(getFlipCompletionSideIndex(input), expected);
+}
+
 test('queue header and reel caption stay on Side A until the flip settles on Side B', () => {
-  assert.equal(
-    getFlipCompletionSideIndex({
+  assertFlipCompletionSideIndex(
+    {
       sideIndex: 0,
       isFlipping: true,
       pendingFlipSideIndex: 1,
-    }),
+    },
     0,
   );
 
-  assert.equal(
-    getFlipCompletionSideIndex({
+  assertFlipCompletionSideIndex(
+    {
       sideIndex: 1,
       isFlipping: true,
       pendingFlipSideIndex: 1,
-    }),
+    },
     0,
   );
 });
 
 test('queue header and reel caption stay on Side B until the flip settles on Side A', () => {
-  assert.equal(
-    getFlipCompletionSideIndex({
+  assertFlipCompletionSideIndex(
+    {
       sideIndex: 1,
       isFlipping: true,
       pendingFlipSideIndex: 0,
-    }),
+    },
     1,
   );
 
-  assert.equal(
-    getFlipCompletionSideIndex({
+  assertFlipCompletionSideIndex(
+    {
       sideIndex: 0,
       isFlipping: true,
       pendingFlipSideIndex: 0,
-    }),
+    },
     1,
   );
 });
 
 test('queue header and reel caption move to Side B once the flip settles on Side B', () => {
-  assert.equal(
-    getFlipCompletionSideIndex({
+  assertFlipCompletionSideIndex(
+    {
       sideIndex: 1,
       isFlipping: false,
       pendingFlipSideIndex: null,
-    }),
+    },
     1,
   );
 });
 
 test('queue header and reel caption move to Side A once the flip settles on Side A', () => {
-  assert.equal(
-    getFlipCompletionSideIndex({
+  assertFlipCompletionSideIndex(
+    {
       sideIndex: 0,
       isFlipping: false,
       pendingFlipSideIndex: null,
-    }),
+    },
     0,
   );
 });
